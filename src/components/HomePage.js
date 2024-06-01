8caa511c5f1af21de72d991773ab29415e72906c
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-01 20:41:39 +1000

    Highlight prime text

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,18 +7,18 @@ function HomePage() {
         <h1>15 hrs! 65 km!</h1>
         <h2>Saturday, 31 August 2024</h2>
         <br></br>
-        <p>Year 1 - Launch walk!</p>
-        <br></br>
-        <p>Start Time: 5:00 AM</p>
-        <p>Finish Time: 8:00 PM</p>
+        <p>
+        <span className="start-point">START: 5 AM </span>
+        <span className="finish-point">FINISH: 8 PM </span>
+        </p>
       </section>
       <section className="section2">
         <div className="content-wrapper">
           <p>Embark on an invigorating journey of health and wellness with the Penrith to Pyrmont walk, 
             a scenic adventure spanning 65 kilometers and taking approximately 15 hours to complete. Designed for the health-conscious and enthusiasts of outdoor exploration, 
             this exhilarating trek traverses major suburbs, picturesque parks, and iconic landmarks. Whether you're seeking to maintain peak fitness or simply revel in the 
-            joy of walking, this trail promises an unforgettable experience. Lace up your shoes, breathe in the fresh air, and discover the transformative power of nature as 
-            you embark on this remarkable journey from Penrith to Pyrmont.
+            joy of walking, this trail promises an unforgettable experience. Also, this is <span class="highlight-text">YEAR 1 of the walk!!</span> So, lace up your shoes, breathe in the fresh air, 
+            and <span class="highlight-text">be a part of history</span> as you embark on this remarkable journey from Penrith to Pyrmont.
           </p>
         </div>
         <img src="/penrith-pyrmont-walk-route-map.png" alt="Route Map" />
