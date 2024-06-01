464c03d41a76e81681fb8f977d64731e6f6f37af
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-01 19:48:39 +1000

    Walk into on homepage and alignment

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,14 +4,22 @@ function HomePage() {
   return (
     <div className="centered-container">
       <section className="section1">
-        <h1>Penrith to Pyrmont Walk</h1>
+        <h1>2024 is now open!</h1>
         <h2>15 hrs! 65 km!</h2>
-        <p>Date: 25th June 2024</p>
+        <p>25th June 2024</p>
+        <br></br>
         <p>Start Time: 5:00 AM</p>
         <p>Finish Time: 8:00 PM</p>
       </section>
       <section className="section2">
-        <p>Come and enjoy a wonderful day walk from Penrith to Pyrmont. This walk will take you through some of the most beautiful landscapes in the region.</p>
+        <div className="content-wrapper">
+          <p>Embark on an invigorating journey of health and wellness with the Penrith to Pyrmont walk, 
+            a scenic adventure spanning 65 kilometers and taking approximately 15 hours to complete. Designed for the health-conscious and enthusiasts of outdoor exploration, 
+            this exhilarating trek traverses major suburbs, picturesque parks, and iconic landmarks. Whether you're seeking to maintain peak fitness or simply revel in the 
+            joy of walking, this trail promises an unforgettable experience. Lace up your shoes, breathe in the fresh air, and discover the transformative power of nature as 
+            you embark on this remarkable journey from Penrith to Pyrmont.
+          </p>
+        </div>
         <img src="/penrith-pyrmont-walk-route-map.png" alt="Route Map" />
         <p>Start Location: Penrith</p>
         <p>End Location: Pyrmont</p>
