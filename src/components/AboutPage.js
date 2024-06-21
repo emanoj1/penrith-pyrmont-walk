279552e1550c20b51cb4f3e996e167029bc6ca8f
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-06-21 13:15:49 +1000

    feat: write About page content

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -13,29 +13,86 @@ const AboutPage = () => {
         <div className="faq">
 
           <h4>What is the Penrith to Pyrmont Walk?</h4>
-          <p>This marathon walk is a personal project of Manoj Kumar, who has spent considerable time in both Penrith and Pyrmont.
-            He used to walk often between Pyrmont & Chatswood, Pyrmont & Summer Hill, and wondered if a walk between Penrith & Pyrmont would be an exciting one!
-            The concept was born in the head sometime in early 2024, and the planning commenced in June 2024. 
-            More about Manoj later in this page!
+          <p>The Penrith to Pyrmont Walk is a personal initiative by Manoj Kumar, who has spent significant time in both Penrith and Pyrmont. 
+            Having frequently walked between Pyrmont & Chatswood and Pyrmont & Summer Hill, Manoj envisioned an even more exciting journey between Penrith & Pyrmont. 
+            This idea took shape in early 2024, and planning commenced in June 2024. 
+            You can learn more about Manoj later on this page!
           </p>
 
           <h4>What is the distance of the walk?</h4>
-          <p>The walk spans 65 kilometers from Penrith to Pyrmont.</p>
+          <p>The walk covers a distance of 65 kilometers from Penrith to Pyrmont.</p>
 
           <h4>How long does it take to complete the walk?</h4>
-          <p>The entire walk takes approximately 15 hours, depending on individual's pace.</p>
+          <p>The walk takes approximately 15 hours non-stop, though this can vary depending on an individual’s pace.</p>
 
           <h4>What is the plan to complete the walk?</h4>
-          <p>The original idea was to complete the walk in a day but then after considerable thought, it was decided to split it over 2 days. 
-            So, the first part of the walk will be on Saturday when we cover Penrith to Parramatta. This will take approximately 8 hours to walk.
-            Then, we meet up next day at Parramatta, when we continue the walk from Parramatta to Pyrmont, which will take approximately 5 hours to walk.
-            We will need to slot in time for breaks, lunch and photos too! It is a long day for sure!
+          <p>Initially, the plan was to complete the walk in one day. However, after careful consideration, it was decided to split it over two days. 
+            On the first day, we will walk from Penrith to Parramatta, which is about 33 km and will take approximately 8 hours. 
+            The next day, we will continue from Parramatta to Pyrmont, covering the remaining 22 km in about 5 hours. 
+            Breaks, lunch, and photo opportunities will be included, making it a full and enjoyable experience!
             </p>
 
-          <h4>Who can participate?</h4>
-          <p>The walk is suitable for individuals over the age of 18 who are medically fit.</p>
+          <h4>What is the walking route?</h4>
+          <p>You can refer to the map on the homepage or search for Penrith Station to Pyrmont Station online to view the route. 
+            In summary, we will be walking along the path adjacent to all the train stations on the way. 
+            This keeps us close to shops, people, and necessary facilities like restrooms.</p>
 
-          {/* Add more FAQs as needed */}
+          <h4>Who can participate?</h4>
+          <p>The walk is open to individuals over 18 years old who are medically fit. 
+            Due to the strenuous nature of the walk, prior experience with long-distance walking is highly recommended. 
+            Participants can rest at their own pace if needed and can discontinue the walk and use public transport if they choose.
+          </p>
+
+          <h4>What should I carry with me?</h4>
+          <p>
+          Here are some recommendations: energy snacks, lunch, a water bottle, a towel, a mobile phone charger and power banks, a travel card for public transport, a hat or cap, 
+          sunscreen, sunglasses, headphones, fitness trackers, and any personal medications. 
+          It's also wise to bring ointments and a medical kit to manage blisters and sore feet.
+          </p>
+
+          <h4>What about footwear?</h4>
+          <p>
+          You will need high-quality long-distance walking shoes. Casual sneakers and regular shoes won’t suffice. 
+          Your shoes should provide stability and have wide soles near the forefoot for toe space. Choose something sturdy and specifically designed for long walks. 
+          The walking path is entirely flat, consisting of cemented footpaths. Also, make sure to wear comfortable socks. 
+          </p>
+
+          <h4>Do I get trophies and certificates?</h4>
+          <p>
+          Currently, there are no plans for trophies due to cost constraints and the lack of a sponsor. 
+          If your business can sponsor trophies, we’d love to feature your logo on our homepage for that year’s event! 
+          Certificates are more affordable and something we might consider. We’ll keep you posted. If you have other ideas, feel free to email us!
+            </p>
+
+          <h4>Who do I contact for further clarifications?</h4>
+          <p>For more information, you can reach out to Manoj via the Contact page, and he will respond to your queries.</p>
+
+          <h4>What else should I know?</h4>
+          <p>This walk is a personal project and not sponsored by any government, fitness body, or charitable organization. 
+            We are a group of enthusiasts who gather to enjoy the walk. As such, we are not responsible for any issues that may arise during the walk, including injuries or illnesses. 
+            In case of emergencies, please contact the relevant authorities, such as the police or ambulance services. 
+          </p>
+
+          <h4>Who is the creator of this event?</h4>
+          <p>My name is Manoj Kumar, and I live in Sydney, Australia. 
+            He has a full-time work in digital technology, and in his spare time he works on various hobbies like below:
+          </p>
+          <br></br>
+          <ol>
+            <li>Learning full-stack web development: HTML/CSS, Python, PostgreSQL, JavaScript, React, NodeJS, and Express</li>
+            <li>Launched <a href="https://education.cv" target="_blank" rel="noopener noreferrer"> education.cv</a> - a website to showcase your studies!</li>
+            <li>Built an API based app, <a href="https://commuter.fyi" target="_blank" rel="noopener noreferrer"> commuter.fyi</a> - a world-wide public transport planner</li>
+            <li>Runs a virtual & private travel assistant at <a href="https://okratravels.com.au" target="_blank" rel="noopener noreferrer"> Okra Travels</a></li>
+            <li>Also, launched <a href="https://fitnessleafai.com" target="_blank" rel="noopener noreferrer"> Fitness Leaf AI</a> - an AI enabled vegetarian recipe generator!</li>
+            <li>Podcasting when time allows at <a href="https://manojspeaks.com" target="_blank" rel="noopener noreferrer"> Manoj Speaks</a></li>
+            <li>When I have much time, another podcast at <a href="https://randomstrangertalk.com" target="_blank" rel="noopener noreferrer"> Random Stranger Talk</a></li>
+            <li>YouTube Vlogging with <a href="https://youtube.com/@checkoutaustralia" target="_blank" rel="noopener noreferrer"> Check Out Australia</a> - exploring Australia</li>
+            <li>Blogging my thoughts at <a href="https://emanoj.com" target="_blank" rel="noopener noreferrer"> emanoj.com</a></li>
+            <li>Published my first book! <a href="https://chesterthedino.com" target="_blank" rel="noopener noreferrer"> Chester, the purple dino! </a></li>
+            <li>Tweeting away at <a href="https://x.com/emanoj_" target="_blank" rel="noopener noreferrer"> @emanoj_</a></li>
+          </ol>
+
+
         </div>
       </div>
     </div>
